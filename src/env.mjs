4948a29d2e3a897562b31f798f217a9fa619ba0e
Eyüb Yıldırım <eyubyildirim@Eyubs-MacBook-Pro.local>
2023-11-06 10:56:06 +0300

    fix: env

--- src/env.mjs
+++ src/env.mjs
@@ -18,6 +18,7 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
+    NEXT_PUBLIC_SPOTIFY_REDIRECT: z.string().url(),
   },
 
   /**
@@ -25,8 +26,8 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_SPOTIFY_REDIRECT: process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT,
+    NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
 });
--- src/util/api/spotify.ts
+++ src/util/api/spotify.ts
@@ -4,6 +4,9 @@ import { env } from "~/env.mjs";
 export const fetchSpotify = async (code: string) => {
   console.log(code);
 
+  console.log("http://localhost:3000/callback");
+  console.log(env.NEXT_PUBLIC_SPOTIFY_REDIRECT);
+
   const result = await axios({
     method: "post",
     url: "https://accounts.spotify.com/api/token",
